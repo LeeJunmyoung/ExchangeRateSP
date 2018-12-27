ee07c5ad702746e83cc884eadbc75fea6d835e6d
leejm1 <wnsaud5415@gamil.com>
2018-12-27 19:31:16 +0900

    first commit
    spring boot 초기 설정.

---
+++ exchageratedemo/src/main/java/com/exchage/exchageratedemo/ExchageratedemoApplication.java
@@ -0,0 +1,14 @@
+package com.exchage.exchageratedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchageratedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchageratedemoApplication.class, args);
+	}
+
+}
+
